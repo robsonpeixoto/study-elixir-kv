7a0dcb0fabbc8f72463ca2b403425e98461f9fe1
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-05-06 10:14:34 -0300

    refactor: avoid repeat the KV.Bucket creation and test key that do not exists

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,10 +1,17 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link([])
-    assert KV.Bucket.get(bucket, "milk") == nil
+    %{bucket: bucket}
+  end
 
+  test "if the key does not exists the bucket will returns `nil`", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "does-not-exists") == nil
+  end
+
+  test "Bucket.put will create a key with the specified value", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
