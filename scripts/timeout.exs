3e75e696185121bcfd6a48b96a32d67f400c9e49
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-05-19 08:16:20 -0300

    chore: add a timeout genserver example

---
+++ scripts/timeout.exs
@@ -0,0 +1,29 @@
+defmodule TimeoutGenServer do
+  use GenServer
+  # Server (callbacks)
+  @impl true
+  def init(_state) do
+    {:ok, "", 1_000}
+  end
+
+  @impl true
+  def handle_call(:hi , _from, _state) do
+    # Process.sleep(3_000)
+    {:reply, "hello", "", 2_000}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    IO.inspect({"handle_info", msg, state})
+    {:noreply, state}
+  end
+end
+
+# Start the server
+{:ok, pid} = GenServer.start_link(TimeoutGenServer, "", timeout: 1_000)
+IO.inspect(pid)
+
+# This is the client
+ret = GenServer.call(pid, :hi, 2_000)
+IO.inspect(ret)
+#=> :hello
