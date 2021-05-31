947f6bf28d52e361272a80ce8e4c0f6b832093b0
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-05-31 09:47:26 -0300

    feature: implement KV.Registry client and add tests

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,7 +1,30 @@
 defmodule KV.Registry do
   use GenServer
 
-  ## TODO: Missing Client API
+  ## Client API
+
+  @doc """
+  Starts the registry
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
 
   ## Defining GenServer Callbacks
 
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,3 +1,18 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
+
+  setup do
+    registry = start_supervised!(KV.Registry)
+    %{registry: registry}
+  end
+
+  test "is possibly create a bucket", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shoppint") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
 end
