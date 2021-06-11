803a5aaa196574c052b7cb141965ad8ad45326d4
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-06-11 20:48:53 -0300

    feature: add a simples route table

---
+++ apps/kv/test/kv/router_test.exs
@@ -0,0 +1,15 @@
+defmodule KV.RouterTest do
+  use ExUnit.Case, async: true
+
+  @tag :distributed
+  test "route requests across nodes" do
+    assert KV.Router.route("hello", Kernel, :node, []) == :foo@updated2019
+    assert KV.Router.route("world", Kernel, :node, []) == :bar@updated2019
+  end
+
+  test "raises on unknown entries" do
+    assert_raise RuntimeError, ~r/cloud not find entry/, fn ->
+      KV.Router.route(<<0>>, Kernel, :node, [])
+    end
+  end
+end
